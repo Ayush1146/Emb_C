70b9729f70daa6a9584c33282c3879ab770c8495
Ayush Sharma <65450162+Ayush1146@users.noreply.github.com>
2021-04-26 10:04:10 +0530

    Add files via upload

--- Activity1/src/main.c
+++ Activity1/src/main.c
@@ -1,50 +1,38 @@
-/**
- * @file main.c
- * @author Ankana (you@domain.com)
- * @brief
- * @version 0.1
- * @date 2021-04-23
- *
- * @copyright Copyright (c) 2021
- *
+/*
  */
-
 
 #include <avr/io.h>
-
-
 #include <util/delay.h>
-/**
- * @brief
- *
- * @return int
- */
+
 int main(void)
 {
+    DDRB |= (1<<PB0);
+
+    DDRD &=~(1<<PD0);
+    DDRD &=~(1<<PD1);
+    PORTD |= (1<<PD0);
+    PORTD |= (1<<PD1);
+
+    while(1){
+    if((!(PIND&(1<<PD0))) && (!(PIND&(1<<PD1))))
+       {
+
+       PORTB|= (1<<PB0);
+       _delay_ms(2000);
+       }
+
+        else{
+
+        PORTB&=~(1<<PB0);
+        _delay_ms(2000);
 
 
-  DDRC|= (1<<PB0); //B0 =1
-  DDRD&=~(1<<PD0);  //
-  PORTD|=(1<<PD0); //D0=1
-  PORTD|=(1<<PD1);
+
+        }
 
 
-  while(1)
-  {
-      if((!(PIND&(1<<PD0)) && (!(PIND&(1<<PD1)))))
-      {
-           PORTB|=(1<<PB0);
-          _delay_ms(2000);
+    }
 
-      }
-      else
-      {
-          PORTB &= ~(1<<PB0);
-          _delay_ms(2000);
-      }
 
-  }
-  return 0;
+    return 0;
 }
-
-
